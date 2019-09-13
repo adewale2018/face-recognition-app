c87b420e84df99ae091ae2b60a66e7e50e880b2c
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2019-09-13 09:59:33 +0100

    Add face detection box and logic

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -9,7 +9,13 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
       </p>
       <div className="center">
         <div className="form pa4 br3 shadow-5 center">
-          <input className="f4 pa2 w-70 center" type="text" onChange={onInputChange}/>
+          <input 
+            className="f4 pa2 w-70 center"
+            type="text" 
+            onChange={onInputChange} 
+            required={'required'}
+
+            />
           <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple"
             onClick={onButtonSubmit}
           >
